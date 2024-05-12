5c6e11740ffa7481b18b08ce2079cad1c65087d7
Muhammad Muzammil <zmuzammil74@gmail.com>
2024-05-12 22:34:23 +0500

    settings updated

--- a_core/settings.py
+++ a_core/settings.py
@@ -156,7 +156,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [ BASE_DIR / 'static' ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
